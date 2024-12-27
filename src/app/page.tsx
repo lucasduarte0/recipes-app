a7d59e3a61e3244af976b658164e7b37be80ec5e
Lucas Duarte <lucasduarte9120@gmail.com>
2024-12-27 09:18:26 -0500

    Simple change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,15 @@
 "use client";
-
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import RecipesCards from "@/components/RecipesCards";
 import Search from "@/components/Search";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useState } from "react";
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState("");
-
+  const [searchTerm, setSearchTerm] = useState<string>("");
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="mx-auto px-4 sm:px-6 lg:px-8 max-w-[1400px] grid place-items-start gap-5 py-8 sm:py-12">
-        <SignedIn>
-          <UserButton
-            appearance={{
-              elements: {
-                userButtonAvatarBox: "size-6",
-              },
-            }}
-          />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <button className="px-4 py-2 rounded-full bg-[#131316] text-white text-sm font-semibold">
-              Sign in
-            </button>
-          </SignInButton>
-        </SignedOut>
         <Navbar />
         <Search value={searchTerm} onChange={setSearchTerm} />
         <RecipesCards searchTerm={searchTerm} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,51 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+// import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+"use client";
+import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
+// import { currentUser } from "@clerk/nextjs/server";
 
 export default function Navbar() {
   return (
+    <div>
+      <SignedIn>
+        <NavbarSignedIn />
+      </SignedIn>
+      <SignedOut>
+        <NavbarSignedOut />
+      </SignedOut>
+    </div>
+  );
+}
+
+function NavbarSignedIn() {
+  // const user = await currentUser();
+
+  return (
     <div className="flex w-full gap-3">
-      <Avatar className="shrink-0 m-auto w-12 h-12">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: "size-12",
+          },
+        }}
+      />
       <div className="flex-1 flex flex-col">
         <h1 className="text-xl font-bold">Hello, World!</h1>
         <p className="text-sm text-muted-foreground">
           Welcome to Next.js with Tailwind CSS
         </p>
       </div>
+    </div>
+  );
+}
+
+function NavbarSignedOut() {
+  return (
+    <div className="flex w-full gap-3">
+      <SignInButton>
+        <button className="px-4 py-2 rounded-full bg-primary text-white text-sm font-semibold">
+          Sign in
+        </button>
+      </SignInButton>
     </div>
   );
 }
